a80534521f8509f31e4d92b3c1822a9cb20f489a
Javier Durán Torres <118077906+TRENCH13@users.noreply.github.com>
2023-06-16 12:49:22 -0600

    Correciones hechas

---
+++ Codigo/SistemaTutorias/ClienteSistemaTutorias/InterfacesUsuario/CUs/WindowReporteTutor.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteSistemaTutorias.InterfacesUsuario
+{
+    /// <summary>
+    /// Interaction logic for WindowReporteTutor.xaml
+    /// </summary>
+    public partial class WindowReporteTutor : Window
+    {
+        public WindowReporteTutor(string descripcion, string comentariosGenerales)
+        {
+            InitializeComponent();
+
+            lbDescripcion.Content = descripcion;
+            lbComentarioGenerales.Content = comentariosGenerales;
+        }
+    }
+}
